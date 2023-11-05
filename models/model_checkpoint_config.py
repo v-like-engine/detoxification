af2e1ca94ef5714a645dfdb3f8fc0d0896176e6c
v-like-engine <cypepv@yandex.ru>
2023-11-05 18:32:06 +0300

    Add config files with setting constants

---
+++ models/model_checkpoint_config.py
@@ -0,0 +1,4 @@
+BERT_BASE = "bert-base-uncased"
+T5_S = "t5-small"
+T5_BASE = "t5-base"
+PROPHET = "microsoft/prophetnet-large-uncased"
---
+++ src/data/config.py
@@ -0,0 +1,8 @@
+PREFIX = "detoxify the following text (make it less toxic while preserving the meaning):"
+REFERENCE = "toxic"
+TRANSLATION = "neutral"
+MAX_INPUT_LENGTH = 128
+MAX_TARGET_LENGTH = 128
+TRAIN = "train"
+VAL = "validation"
+TEST = "test"
